48e998147708cffd37ef67faad46045c166f9b19
stupot1 <scpitc@gmail.com>
2020-07-14 15:28:35 +0100

    Signup form changed to post request through htmlm form, jquery on from removed, changed 'test' to 'main'

--- makersbnb/app.js
+++ makersbnb/app.js
@@ -3,21 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+//app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use('/', indexRouter);
 
@@ -25,9 +28,18 @@ app.get('/', (req,res)=> { //get method
   res.sendFile(path.join(__dirname + '/views/index.html')); //send response
 })
 
-app.post('/test', (req,res)=> { //get method
-  //console.log(params);
-  res.sendFile(path.join(__dirname + '/views/test.html')); //send response
+app.post('/main', (req,res)=> { //post method
+  //console.log(req.body.name);
+  var name = req.body.name;
+  var email = req.body.email;
+  var password = req.body.password;
+  //res.sendStatus(200);
+  //Users.create(name, email, password)
+  res.sendFile(path.join(__dirname + '/views/main.html')); //send response
+})
+
+app.get('/main', (req,res)=> {
+  res.sendFile(path.join(__dirname + '/views/main.html')); //send response
 })
 
 
